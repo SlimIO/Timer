1bea12144c311bf02f05315b2c302cce9fab089a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-23 22:10:27 +0200

    feat: initial prototype

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,11 @@
 /// <reference types="@types/node" />
 
 declare namespace Timer {
+    type fn = (...any) => void;
 
-
+    export function setInterval(fn: fn, delayMs: number, ...params: any[]): number;
+    export function setTimeout(fn: fn, delayMs: number, ...params: any[]): number;
+    export function clearTimer(id: number): number;
 }
 
 export as namespace Timer;
---
+++ index.js
@@ -0,0 +1,110 @@
+// Require NodeJS Dependencies
+const nodeTimer = require("timers");
+
+// CONSTANTS
+const THRESHOLD_MS = 1;
+const AGGRESSION_RATE = 1.1;
+
+/**
+ * @const timeHandles
+ * @type {Map<Number, NodeJS.Timeout>}
+ * @desc Map of timeOut handles for Interval and Timeout
+ */
+const timeHandles = new Map();
+
+// Timeout and Interval ID
+let currTimeId = 0;
+
+/**
+ * @function now
+ * @desc Return the current time with no clock drift
+ * @return {Number}
+ */
+function now() {
+    const [seconds, nanoseconds] = process.hrtime();
+
+    return seconds * 1e3 + nanoseconds / 1e6;
+}
+
+/**
+ * @function setTimer
+ * @desc Set a new aggressive timeOut timer
+ * @param {!Number} id Timer handle id
+ * @param {*} opts timer options
+ * @param {Number} opts.executedAt The timestamp at which timer should be executed
+ * @param {*} opts.fn The callback that should be executed when the time is elapsed
+ * @returns {void}
+ */
+function setTimer(id, opts) {
+    /** @type {NodeJS.Timeout} */
+    let timer;
+
+    const delayMs = opts.executeAt - now();
+    if (delayMs > THRESHOLD_MS) {
+        timer = nodeTimer.setTimeout(() => setTimer(id, opts), delayMs / AGGRESSION_RATE);
+    }
+    else {
+        timer = nodeTimer.setTimeout(() => opts.fn(), 0);
+    }
+
+    timeHandles.set(id, timer);
+}
+
+/**
+ * @function clearTimer
+ * @desc Clear a timer (interval or timeout) with his id.
+ * @param {!Number} id Time handle id
+ * @returns {void}
+ */
+function clearTimer(id) {
+    if (timeHandles.has(id)) {
+        nodeTimer.clearTimeout(timeHandles.get(id));
+    }
+}
+
+/**
+ * @function setTimeout
+ * @desc Same as NodeJS.setTimeout with no Drift
+ * @param {*} callFN Function handler that should be executed after delayMs
+ * @param {!Number} delayMs Delay in milliseconds before callFN execution
+ * @param  {...any} params Handler parameters
+ * @returns {Number}
+ */
+function setTimeout(callFN, delayMs = 1, ...params) {
+    const id = currTimeId++;
+    setTimer(id, {
+        executeAt: now() + delayMs,
+        fn: () => callFN.call(null, ...params)
+    });
+
+    return id;
+}
+
+/**
+ * @function setInterval
+ * @desc Same as NodeJS.setInterval with no Drift
+ * @param {*} callFN Function handler that should be executed after delayMs
+ * @param {!Number} delayMs Delay in milliseconds before callFN execution
+ * @param  {...any} params Handler parameters
+ * @returns {Number}
+ */
+function setInterval(callFN, delayMs = 1, ...params) {
+    const id = currTimeId++;
+    setTimer(id, {
+        executeAt: now() + delayMs,
+        fn() {
+            this.executeAt += delayMs;
+            setTimer(id, this);
+
+            return callFN(...params);
+        }
+    });
+
+    return id;
+}
+
+module.exports = {
+    setInterval,
+    setTimeout,
+    clearTimer
+};
