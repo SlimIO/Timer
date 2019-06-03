e86c67a6a00d7ee2fd03bbffa50a7bdc908faeec
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-06-03 17:22:31 +0200

    test: coverage 100%

--- test/test.js
+++ test/test.js
@@ -17,3 +17,27 @@ japaTest("setTimeout", async(assert) => {
         }, 202);
     });
 });
+
+japaTest("setTimeout without time", async(assert) => {
+    let count = 0;
+    function callback() {
+        count++;
+    }
+
+    const timerInterval = Timer.setInterval(callback);
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            assert.equal(count, 2);
+            Timer.clearInterval(timerInterval);
+            console.log("test");
+            resolve();
+        }, 4);
+    });
+});
+
+japaTest("clear unexistant interval", async(assert) => {
+    Timer.clearInterval(-2);
+    Timer.clearInterval("");
+    Timer.clearInterval({});
+    Timer.clearInterval([]);
+});
